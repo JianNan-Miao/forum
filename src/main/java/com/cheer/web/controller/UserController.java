efb6bdec290a9597321d23310a747ddd461034f8
1310730057@qq.com <88336261mjn>
2019-07-13 13:06:20 +0800

    update

--- src/main/java/com/cheer/web/controller/UserController.java
+++ src/main/java/com/cheer/web/controller/UserController.java
@@ -19,7 +19,6 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
